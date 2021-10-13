c0a8f184f82daa4bcc6766b5834a9c17421dab0b
Tan Sook Mun <stan0111@student.monash.edu>
2021-10-13 17:42:03 +0800

    created radial and bar chart

--- Data/check.py
+++ Data/check.py
@@ -1,6 +1,7 @@
 import pandas as pd
-df= pd.read_csv("world-happiness-report-2021.csv")
-highest=df["Logged_GDP_per_capita"].max()
-lowest=df["Logged_GDP_per_capita"].min()
+# df= pd.read_csv("world-happiness-report-2021.csv")
+df= pd.read_csv("2020.csv")
+highest=df["Healthy life expectancy"].max()
+lowest=df["Healthy life expectancy"].min()
 print(highest)
 print(lowest)
