4d1864f96a6cae8546b5f0e56ad5026f78c6535d
Tan Sook Mun <stan0111@student.monash.edu>
2021-10-16 15:20:56 +0800

    refractor my layout

--- Data/check.py
+++ Data/check.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import csv
 # df= pd.read_csv("world-happiness-report-2021.csv")
-df= pd.read_csv("world-happiness-report-2021.csv")
-highest=df["Healthy_Life_Expectancy"].max()
-lowest=df["Healthy_Life_Expectancy"].min()
-print(highest)
-print(lowest)
+# df= pd.read_csv("world-happiness-report-2021.csv")
+# highest=df["Healthy_Life_Expectancy"].max()
+# lowest=df["Healthy_Life_Expectancy"].min()
+df= pd.read_csv("WHOdata.csv")
+# new_dataframe = df.filter(["Country","Year","Both sexes"], axis=1)
+new_dataframe = df[df.Year.eq(2015)]
+# newdf=df[df["Country","Year","Both sexes"]]
+# newdf=df["Country","Year","Both sexes"].head()
+print(new_dataframe)
+new_dataframe.to_csv('2015lifeexpect.csv')
